ab18f52c600b3ac5a84a361e68845b2c30cfeb48
wyattjsmith <wysmith@232x20.csc.calpoly.edu>
2013-10-31 17:00:13 -0700

    Added Preliminary Database Manager

---
+++ Tic Talk/DBManager.h
@@ -0,0 +1,22 @@
+//
+//  DBManager.h
+//  Tic Talk
+//
+//  Created by Wyatt Smith on 10/29/13.
+//  Copyright (c) 2013 Wyatt Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+#import "Activity.h"
+
+@interface DBManager : NSObject
+
+@property (nonatomic) NSString *dbPath;
+@property (nonatomic) sqlite3 *tiktalkDB;
+
+-(void) initDB;
+-(void) insertObject:(Activity *) activity;
+-(Activity *) queryActivities:(NSString *) query;
+
+@end
